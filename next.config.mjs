5aaaa2c28d782f744e01e41c9704d63499760839
amanjoshi2002 <sctaman21@gmail.com>
2025-01-12 15:11:44 +0530

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   output: 'standalone', // or 'export' for static sites
   images: {
     remotePatterns: [
